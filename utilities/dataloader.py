7b30d3a231cf5b97146eb474e7058e9c890fd70a
Abeldewit <abeldewit@gmail.com>
2022-05-18 18:43:10 +0200

    Added documentation and faster data loading for non temporal

--- utilities/dataloader.py
+++ utilities/dataloader.py
@@ -6,6 +6,8 @@ import torch
 import pathlib
 from utilities.util_funcs import pad_tensor
 from pipe import chain
+from ast import literal_eval as eval
+from functools import lru_cache
 
 def create_split_dataloaders(
     dataset: tdata.Dataset, 
@@ -34,13 +36,24 @@ def create_split_dataloaders(
     return train_set, val_test, test_set
 
 class PASTIS(tdata.Dataset):
-    def __init__(self, path_to_pastis:str, data_files: str, label_files: str, pad: bool=False, rgb_only: bool=False, no_time: bool = True) -> None:
+    """
+    Data loader for PASTIS dataset. With customization options for the data output. 
+    Args:
+        path_to_pastis: path to the folder containing the data
+        data_files: path to the file containing the data
+        label_files: path to the file containing the labels
+        pad: whether to pad the tensor to the max_t
+        rgb_only: whether to only use the RGB channel
+        multi_temporal: whether to use multi-temporal data
+
+    """
+    def __init__(self, path_to_pastis:str, data_files: str, label_files: str, pad: bool=False, rgb_only: bool=False, multi_temporal: bool = True) -> None:
         # Path and folder names
         self.folder = path_to_pastis
         self.data_files = data_files
         self.label_files = label_files
         self.rgb = rgb_only
-        self.no_time = no_time
+        self.multi_temporal = multi_temporal
 
         # File structure with path and file names
         self.__file_structure = {
@@ -68,7 +81,7 @@ class PASTIS(tdata.Dataset):
         if self.counter >= len(self):
             raise StopIteration
         else:
-            if self.no_time:
+            if not self.multi_temporal:
                 x_path, y_path, time = self.combination[self.counter]
             else:
                 x_path, y_path = self.combination[self.counter]
@@ -82,14 +95,14 @@ class PASTIS(tdata.Dataset):
             if self.rgb:
                 x = x[:, [2, 1, 0], :, :]
 
-            if self.no_time:
+            if not self.multi_temporal:
                 x = x[time, :]
             
             self.counter += 1
             return (pad_tensor(x, self.max_t, pad_value=0), y) if self.pad else (x, y)
     
     def __getitem__(self, item):
-        if self.no_time:
+        if not self.multi_temporal:
             x_path, y_path, time = self.combination[item]
         else:
             x_path, y_path = self.combination[item]
@@ -103,34 +116,48 @@ class PASTIS(tdata.Dataset):
         if self.rgb:
             x = x[:, [2, 1, 0], :, :]
         
-        if self.no_time:
+        if not self.multi_temporal:
             x = x[time, :]
         
         return (pad_tensor(x, self.max_t, pad_value=0), y) if self.pad else (x, y)
 
-
-    def create_combination(self):
-        # Get the path to the wanted files
-        data_structure = self.__file_structure[self.data_files]
-        label_structure = self.__file_structure[self.label_files]
-
-        # Get the file names
-        x_values = list(data_structure[0].glob(data_structure[1]))
-        y_values = list(label_structure[0].glob(label_structure[1]))
-
-        # Get the file name ID's 
-        x_ids = {x.name.split('_')[-1].split('.')[0]: x for x in x_values}
-        y_ids = {y.name.split('_')[-1].split('.')[0]: y for y in y_values}
-
-        # Assert if there is the same amount of files
-        assert len(set(x_ids.keys()) - set(y_ids.keys())) == len(x_values) - len(y_values)
-
-        # Create a set of the ID's that both folders share
-        common_ids = list(set(x_ids.keys()) - set(set(x_ids.keys()) - set(y_ids.keys())))
-        # Use the common ids to create a combination of the files
-        combined = [(x_ids[idx], y_ids[idx]) for idx in common_ids]
+    @lru_cache(maxsize=None)
+    def create_combination(self) -> list:
+        # Only check is whether to use time or not
+        if self.multi_temporal:
+            _file = './utilities/time.txt'
+        else:
+            _file = './utilities/no_time.txt'
         
-        # If not using time steps, create single samples of each time step (takes a while)
-        if self.no_time:
-            combined = list([[(combi[0], combi[1], i) for i in range(np.load(combi[0]).shape[0])] for combi in combined] | chain)
-        return combined
+        with open(_file, 'r') as f:
+            time_combined = [eval(line) for line in f.readlines()]
+            return time_combined
+
+    # def create_combination_old(self):
+    #     ## OBSCURE FUNCTION, DO NOT USE
+    #     ## PATHS AND FILE NAMES ARE HARDCODED
+
+    #     # Get the path to the wanted files
+    #     data_structure = self.__file_structure[self.data_files]
+    #     label_structure = self.__file_structure[self.label_files]
+
+    #     # Get the file names
+    #     x_values = list(data_structure[0].glob(data_structure[1]))
+    #     y_values = list(label_structure[0].glob(label_structure[1]))
+
+    #     # Get the file name ID's 
+    #     x_ids = {x.name.split('_')[-1].split('.')[0]: x for x in x_values}
+    #     y_ids = {y.name.split('_')[-1].split('.')[0]: y for y in y_values}
+
+    #     # Assert if there is the same amount of files
+    #     assert len(set(x_ids.keys()) - set(y_ids.keys())) == len(x_values) - len(y_values)
+
+    #     # Create a set of the ID's that both folders share
+    #     common_ids = list(set(x_ids.keys()) - set(set(x_ids.keys()) - set(y_ids.keys())))
+    #     # Use the common ids to create a combination of the files
+    #     combined = [(x_ids[idx], y_ids[idx]) for idx in common_ids]
+        
+    #     # If not using time steps, create single samples of each time step (takes a while)
+    #     if self.no_time:
+    #         combined = list([[(combi[0], combi[1], i) for i in range(np.load(combi[0]).shape[0])] for combi in combined] | chain)
+    #     return combined
