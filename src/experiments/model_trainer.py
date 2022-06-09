f12dd2e85e19d0db3085ec7beb20451ab2f6be53
Abeldewit <abeldewit@gmail.com>
2022-06-09 16:15:59 +0200

    stupid gitignore

---
+++ src/experiments/model_trainer.py
@@ -0,0 +1,159 @@
+import torch
+from tqdm import tqdm
+
+from src.backbones.UTAE.utae import UTAE
+
+def train_model(
+    model, 
+    optimizer, 
+    loss_function, 
+    n_epochs, 
+    batch_size,
+    train_loader, 
+    val_loader, 
+    device, 
+    log_dir,
+    save_dir,
+    ):
+    """
+    Trains the given model for a given number of epochs.
+    """
+    #TODO: Create log writer
+
+    # Train the model
+    for epoch in tqdm(range(n_epochs), desc='Training'):
+        # Train the model
+        model.train(True)
+        # Run the training epoch
+        avg_loss = one_epoch(
+            model=model, 
+            optimizer=optimizer, 
+            loss_function=loss_function, 
+            data_loader=train_loader, 
+            batch_size=batch_size, 
+            device=device
+            )
+
+        # Run the validation epoch 
+        model.eval()
+        val_loss = one_epoch(
+            model=model,
+            optimizer=optimizer,
+            loss_function=loss_function,
+            data_loader=val_loader,
+            batch_size=batch_size,
+            device=device,
+            validation=True
+        )
+        print('Epoch {}: train loss: {}, val loss: {}'.format(epoch + 1, avg_loss, val_loss))
+        save_model(model, epoch, optimizer, avg_loss, save_dir)
+
+def one_epoch(
+    model, 
+    optimizer, 
+    loss_function, 
+    data_loader, 
+    batch_size,
+    device,
+    validation=False
+    ):
+    """
+    Runs one epoch of training.
+    """
+    # Init tracking variables
+    avg_loss = 0.
+    running_loss = 0.
+
+    # Get the number of batches
+    n_batches = len(data_loader) // batch_size + 1
+    batch_iterator = batch_maker(data_loader, batch_size, n_batches)
+
+    for i, data in tqdm(enumerate(batch_iterator), total=n_batches, desc='Training epoch'): 
+        # Get the inputs and labels
+        inputs, labels, time = data
+        # Move the batch to the device
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # Clear the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        if not validation:
+            if type(model) == UTAE:
+                outputs = model(inputs, batch_positions=time)
+            else:
+                outputs = model(inputs)
+
+            # Compute the loss
+            loss = loss_function(outputs, labels.long())
+            loss.backward()
+            # Update the weights
+            optimizer.step()
+        else:
+            with torch.no_grad():
+                if type(model) == UTAE:
+                    outputs = model(inputs, batch_positions=time)
+                else:
+                    outputs = model(inputs)
+                # Compute the loss
+                loss = loss_function(outputs, labels.long())
+
+        # Update the tracking variables
+        running_loss += loss.item()
+        avg_loss += loss.item()
+        if i % batch_size == batch_size -1:
+            last_loss = running_loss / batch_size
+            #TODO: Log the loss
+            if not validation: 
+                print(' \tbatch {} loss: {}'.format(i + 1, last_loss))
+            running_loss = 0.
+        
+        # Garbage collection
+        del inputs, labels, outputs, loss
+        if device.type == 'cuda':
+            torch.cuda.empty_cache()
+
+    return avg_loss / (i + 1)
+
+def batch_maker(data_set, batch_size, n_batches):
+    """
+    Creates a generator that yields batches of data.
+    """
+    
+    # Create an iterator for the data
+    data_iterator = iter(data_set)
+
+    # Create a generator that yields batches
+    for _ in range(n_batches):
+        # Create a list to hold the samples in the batch
+        single_inputs = []
+        single_labels = []
+        times = []
+        # Get the next batch
+        for _ in range(batch_size):
+            try:
+                single_input, single_label, single_time = next(data_iterator)
+            except StopIteration:
+                break
+            # Add the sample to the batch
+            single_inputs.append(single_input)
+            single_labels.append(single_label)
+            times.append(single_time)
+        
+        # Stack the single samples into a batch
+        inputs = torch.stack(single_inputs)
+        labels = torch.stack(single_labels)
+
+        # Garbage collection
+        del single_inputs, single_labels
+        # Return the batch
+        yield inputs, labels, times
+
+def save_model(model, epoch, optimizer, loss, save_dir):
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+            }, save_dir)
---
+++ src/terminal_training.py
@@ -0,0 +1,114 @@
+import torch
+import sys, os
+
+sys.path.insert(0, os.getcwd())
+from src.backbones.UNet.unet import UNet
+from src.backbones.UTAE.utae import UTAE
+from src.backbones.Vit.model.vit import VisionTransformer
+from src.backbones.Vit.model.decoder import MaskTransformer
+from src.backbones.Vit.model.segmenter import Segmenter
+from src.experiments.model_trainer import train_model
+from src.utilities.dataloader import PASTIS
+del sys.path[0]
+import json
+import random 
+
+# PARAMETERS
+BATCH_SIZE = 32
+EPOCHS = 10
+learning_rate = 0.01
+SHUFFLE = True
+MODEL_DIR = './models/'
+LOG_DIR = './logs/tensorboard/'
+
+# Standard arguments for every experiment
+STD_ARGS = {
+        'path_to_pastis':'src/data/PASTIS', 
+        'data_files': 'DATA_S2', 
+        'label_files':'ANNOTATIONS',
+}
+
+# Create folders for models and logs
+if not os.path.exists(MODEL_DIR):
+    os.makedirs(MODEL_DIR)
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+
+# Read in the experiments
+with open(os.path.join(os.getcwd(), 'src/experiments/experiments.json'), 'r') as f:
+    experiments = json.load(f)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+models = {
+    'UNet': UNet,
+    'UTAE': UTAE,
+    'ViT': {
+        'encoder': VisionTransformer, 
+        'decoder': MaskTransformer, 
+        'segmenter': Segmenter
+        },
+}
+
+
+for exp, args in list(experiments.items()):
+    print(f'\n\n** Running: {exp} **')
+    print('-'*27)
+    # Get the arguments
+    model_name = args['model']
+    print(f'Model: {model_name}')
+
+    data_options = {
+        'rgb_only': args['rgb_only'],
+        'multi_temporal': args['multi_temporal'],
+        'fold': args['fold'],
+    }
+
+    print('Options:\n  {}'.format('\n  '.join(['{}: {}'.format(k, v) for k, v in data_options.items()])))
+    print('-'*27)
+
+    # Create the dataloaders
+    train_loader = PASTIS(**STD_ARGS, **data_options, shuffle=True, subset_type='train')
+    val_loader = PASTIS(**STD_ARGS, **data_options, shuffle=True, subset_type='val')
+    test_loader = PASTIS(**STD_ARGS, **data_options, shuffle=True, subset_type='test')
+
+    # Creating the model
+    if model_name != 'ViT':  # Vit needs some special attention
+        model_options = {}
+        model_options.update(args['standard_arguments'])
+        model_options.update(args['special_arguments'])
+
+        model = models[model_name](**model_options)
+    else:
+        encoder_options = args['standard_arguments']['encoder']
+        decoder_options = args['standard_arguments']['decoder']
+        segmenter_options = args['standard_arguments']['segmenter']
+        encoder_options.update(args['special_arguments']['encoder'])
+
+        model = models[model_name]['segmenter'](
+            encoder=models[model_name]['encoder'](**encoder_options),
+            decoder=models[model_name]['decoder'](**decoder_options),
+            **segmenter_options
+        )
+
+    model.to(device)
+
+    # Create the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # Create the loss function
+    loss_function = torch.nn.CrossEntropyLoss(label_smoothing=.1)
+
+    # Train the model
+    train_model(
+        model=model,
+        optimizer=optimizer,
+        loss_function=loss_function,
+        n_epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        device=device,
+        log_dir=None,
+        save_dir='./models/',
+    )
