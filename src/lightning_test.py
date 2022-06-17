3d274f7c60073526055eac0d736dac5c50ce33c5
Abeldewit <abeldewit@gmail.com>
2022-06-17 19:56:30 +0200

    typo in config_optim

--- src/lightning_test.py
+++ src/lightning_test.py
@@ -80,9 +80,9 @@ class LiTUNet(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, verbose=True)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
 
-        return [optimizer], [{"scheduler": scheduler, "interval": "epoch", "monitor": "val_loss"}]
+        return [optimizer], [{"lr_scheduler": scheduler, "interval": "epoch", "monitor": "val_loss"}]
     
     def training_step(self, train_batch, batch_idx):
         inputs, labels, times = train_batch
