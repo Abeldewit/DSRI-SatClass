d0cf2240a6263b32967268886b24716d30b0d86e
Abeldewit <abeldewit@gmail.com>
2022-06-21 13:47:16 +0200

    Changed early stopping to loss/val

--- src/main.py
+++ src/main.py
@@ -76,7 +76,7 @@ def create_model(model_name, args):
 
 def create_trainer(hparams):
     early_stopping = EarlyStopping(
-        monitor="Loss/val_epoch", 
+        monitor="Loss/val", 
         mode="max", 
         patience=hparams.patience,
         check_on_train_epoch_end=False,)
